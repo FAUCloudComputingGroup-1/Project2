61de1c81fba891274700c523a2734565763081dd
Okhan2021 <121199592+Okhan2021@users.noreply.github.com>
2023-02-26 15:08:44 -0500

    fixed null error

--- main.py
+++ main.py
@@ -32,23 +32,25 @@ def index():
     s3_client = boto3.client('s3', aws_access_key_id='AKIAWZATVY7MB4BXAOIG', aws_secret_access_key= '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')
     file_names = []
     response = s3_client.list_objects_v2(Bucket="chefomardee-testing")
-    for obj in response['Contents']:
-        file_names.append(obj['Key'])
-    print(file_names)
-    for i in (file_names):
-        s3_client.download_file("chefomardee-testing", i, './files2/'+i)  
-    
-    for file in list_files():
-        index_html += "<li><a href=\"/files2/" + file + "\">" + file + "</a></li>"
+    try:
+        for obj in response['Contents']:
+            file_names.append(obj['Key'])
+        print(file_names)
+        for i in (file_names):
+            s3_client.download_file("chefomardee-testing", i, './files2/'+i)  
+        
+        for file in list_files():
+            index_html += "<li><a href=\"/files2/" + file + "\">" + file + "</a></li>"
 
-    return index_html
+        return index_html
+    except:
+        return index_html
 
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     # print('upload')
     file = request.files['form_file'] 
-
     file.save(os.path.join("./files", file.filename))
     
     s3_client = boto3.client('s3', aws_access_key_id='AKIAWZATVY7MB4BXAOIG', aws_secret_access_key= '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')
