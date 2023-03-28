b90143c01dcbfc20bbdc25a9f03a16389fdd528f
Okhan2021 <121199592+Okhan2021@users.noreply.github.com>
2023-03-28 10:53:30 -0400

    removed title

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ def index():
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <title>Blue</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   </head>
   <body class="bg-light">
